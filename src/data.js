d0177f49bb2cb707718a8db5f4c4d8cc2839b761
Laboratória <gabrielav22@outlook.com>
2019-09-18 14:18:03 -0300

    layoutGabi and cardsTay

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
+window.onload = () => filterItens();
 
+// filtro para a barra de busca unitária por nome
+const inputSearch = searchPoke($search.value, getName);
+
+function searchPoke (inputSearch , getName){
+  const result = getName.filter((poke) => {
+    return poke.name.toLowerCase().match(inputSearch.toLowerCase())
+  });
+  return result
+}
+/*
 function getName(name){
-  const results = POKEMON.pokemon.map(persona => persona.name === name );
+  const results = POKEMON.pokemon.map(item => item.name);
   return results;
 }
-//function searchName(name){
-//  const results = POKEMON.pokemon.filter(procNome => procNome.name === name.value)
-//}
-
+const data = POKEMON.pokemon;
+function filterTypes(data, name){
+  return data.filter(item => item.type.includes(name));
+}*/
--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
-document.getElementById("btn-name").addEventListener("click", filterItens);
+const data = POKEMON.pokemon;
+//const filtro = document.getElementById("")
 
-
-const teste = POKEMON.pokemon;
 function filterItens(){
-    const x = document.getElementById("listaNomes");
-    const y = teste.map(poke => x.innerHTML  += `${poke.id} ${poke.name} ${poke.type} <img src="${poke.img}" /> </br>`);
+    const x = document.getElementById("listaCards");
+    const y = data.map(poke => x.innerHTML  +=
+        `<div class = "box">
+            <h3 class = "namePoke"> ${poke.name}</>
+            <h4> <img class= "imagePoke" src= ${poke.img}> </>
+            <div class = "conteudoCard">
+                <fieldset>
+                    <p>Tipo: ${poke.type} </>
+                    <p>Ovo: ${poke.egg} </>
+                    <p>Chance: ${poke.avg_spawns} % </>
+                    <p>Fraquezas: ${poke.weaknesses}</>
+                </fieldset>    
+            </div>
+        </div>`);
 }
+/*
+const $search = document.querySelector("#pesquisa");
 
+$search.addEventListener("input", filterItens);
+  */  

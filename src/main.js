5780313dbd01de8157b3e531c5851327c4e418a2
Laboratória <gabrielav22@outlook.com>
2019-09-26 18:11:49 -0300

    Test done and image

--- src/main.js
+++ src/main.js
@@ -12,26 +12,24 @@ const dataPoke = POKEMON.pokemon;
 
 // Função para filtrar itens para formar o card
 function showCards(data) {
-  divCards.innerHTML = data.map(poke => {
-    return `
-        <div class = "box">
-            <h3 class = "namePoke"> ${poke.name}</>
-            <h4> <img class= "imagePoke" src= ${poke.img}> </>
-            <div class = "conteudoCard">
-                    <p>Tipo: ${poke.type.join(", ")} </>
-                    <p>Ovo: ${poke.egg} </>
-                    <p>Chance: ${poke.avg_spawns} % </>
-                    <p>Fraquezas: ${poke.weaknesses.join(", ")}</>   
-            </div>
-        </div>`;
-  }).join("");
+  divCards.innerHTML = data.map(poke => `
+    <div class="box">
+      <h3 class="namePoke"> ${poke.name}</h3>
+      <h4> <img class="imagePoke" src=${poke.img} /> </h4>
+      <div class="conteudoCard">
+        <p>Tipo: ${poke.type.join(", ")}</p>
+        <p>Ovo: ${poke.egg}</p>
+        <p>Chance: ${poke.avg_spawns} % </p>
+        <p>Fraquezas: ${poke.weaknesses.join(", ")}</p>   
+      </div>
+    </div>
+  `).join("");
 }
 
 // Ordenação pelo select-name
-selectOrder.addEventListener("change", () => {
-  const selectValue = selectOrder.value;
-  const functionOrdena = window.sortData(dataPoke, "name", selectValue);
-  if (selectValue === "none") {
+selectOrder.addEventListener("change", ({ target: { value } }) => {
+  const functionOrdena = window.sortData(dataPoke, "name", value);
+  if (value === "none") {
     showCards(dataPoke);
   } else {
     showCards(functionOrdena);
@@ -40,9 +38,12 @@ selectOrder.addEventListener("change", () => {
 
 // função para filtrar por tipo com cálculo da porcentagem
 divFilter.addEventListener("change", () => {
-  const filtered= app.filterTypes(dataPoke, divFilter.value);
+  const filtered = app.filterTypes(dataPoke, divFilter.value);
   showCards(filtered);
-  stats.innerHTML=`<p>Os pokémon do tipo ${divFilter.value} representam ${statistics(filtered, dataPoke)}% dos pokemóns da primeira geração.</>`;
+  stats.innerHTML = `
+    <p>
+      Os pokémon do tipo ${divFilter.value} representam ${statistics(filtered, dataPoke)}% dos pokemóns da primeira geração.
+    </p>`;
 });
 
 // evento select para o filtro de tipos
